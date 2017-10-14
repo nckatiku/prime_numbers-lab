59ae6c4486daefa145a67babf5567e0b75957250
Cliff Katiku <ckatiku@SamaDc.corp>
2017-10-14 06:58:49 +0300

    [PrimeNumber function] Add PrimeNumber Function

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,12 @@
+import math
 
+def primeNumbers(n):
+    for i in range(2, int(math.sqrt(n)+1)):
+        if n % i == 0: 
+            return False;
+    return n>1;
+
+print (2)
+for n in range(3, 50):
+    if primeNumbers(n):
+        print(n)
